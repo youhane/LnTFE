996dadf84c6d869a6902b161d8d02c266fe2babf
youhane <jason.sebastian154@gmail.com>
2022-01-07 13:04:28 +0700

    Meet 6

---
+++ Meet6/Grid/script.js
@@ -0,0 +1,52 @@
+console.log("Ini dari script.js")
+
+document.getElementById('angka1').innerHTML = "ini dari scrip"
+
+// variable
+// tipe data
+// function
+
+// integer
+// string
+// char
+// float
+
+// String
+// 12373918230
+// 0813942
+// +6290123182AIYGHKASJJKL
+
+// var, let, const
+
+var dua = 2
+console.log(dua)
+dua = 20
+console.log(dua)
+console.log(dua + 2)
+console.log(dua * 2)
+console.log(dua / 2)
+console.log(dua % 2) // sisa bagi, 20 bagi 2, sisanya gaada = 0
+dua = "kalimat"
+console.log(dua)
+// console.log(dua * 2)
+// NAN = not a number
+
+const konstan = 100
+console.log(konstan)
+// konstan = 200
+console.log(konstan)
+
+const nama = "Jason"
+console.log(nama)
+
+for (let index = 0; index < 10; index++) {
+    console.log("Ini index ke-" + index)
+    {
+        console.log(index)
+    }
+}
+
+// console.log(index)
+
+const angka1 = document.getElementById('angka1')
+angka1.style.width = "100rem"
